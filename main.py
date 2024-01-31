e253160e22270ee3b8b4197ffe5b463637684a1e
Japanjot Singh <123179832+sjapanjots@users.noreply.github.com>
2024-02-01 01:41:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ if st.session_state.clicked[1]:
            st.write(df.describe())  
            #check the correlation values in the dataset
            #st.write("The correlation values in your dataset are:")
-           #correlation =  pd.DataFrame(df.corr()) 
-           #st.write(correlation)
+           correlation =  pd.DataFrame(df.corr()) 
+           st.write(correlation)
            #check the skewness values in the dataset
            st.write("The skewness values in your dataset are:")
            st.write(df.skew()) 
